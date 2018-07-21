cd398e1bad54dc5ce617b6a570ff688ef6b48344
Shivam <shivarawat24@gmail.com>
2018-07-21 00:03:16 -0400

    Started using jwt for authentication and bcrypt for hashing

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
 const Users = require('../modals/user');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 /**
  * @augments: {
  *  username: value,
@@ -11,36 +12,62 @@ const Users = require('../modals/user');
  * }
  * @returns: User
  */
-userRouter.post('/',function(req,res){
+userRouter.post('/register',function(req,res, next){
+    req.body.password = bcrypt.hashSync(req.body.password, 8);
     const user = new Users(req.body);
     user.save(function(err,doc){
-        if (err) throw err;
-        res.json(doc);
-    })
-})
-
-/**
- * @returns: Array of all USERS.
- */
-userRouter.get('/',function(req,res){
-    Users.find({},function(err,users) {
-        if (err) throw err;
-        res.json(users);
+        if (err) next(err);
+        const token = jwt.sign({id: doc._id}, process.env.HMACSECRET,{
+            expiresIn: 8640
+        }); 
+        res.json({auth: true, token});
     });
 })
 
 /**
- * @augments: _id of User
- * @returns: User with specific id
+ * @argument: Access token
+ * @returns: Object of user without password
  */
-userRouter.get('/:id', function(req,res) {
-    Users.findOne({
-        _id: req.params.id
-    }, function(err,result) {
-            if (err) throw err;
+userRouter.get('/',function(req,res, next){
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        res.status(401).json({auth: false, message: 'Token not provided'});
+    }
+
+    jwt.verify(token, process.env.HMACSECRET, function(err, decoded) {
+        if (err) res.status(401).json({auth: false, message:'Can not verify token'});
+        Users.findById(decoded.id,
+            {password: 0},
+            function(err, result) {
+            if (err) res.status(500).send('There was problem finding a user');
+            if (!result) {
+                res.send(500).send("Could not find the user");
+            }
             res.json(result);
+        })
+    })
+})
+
+/**
+ * @argument: email and password
+ */
+userRouter.get('/login', function(req,res,next) {
+    Users.findOne({email: req.body.email},function(err,user) {
+        if (err) res.status(500).send("There was a problem fetching user");
+        if (!user) {
+            res.send(401).send('You have not registered');
+        }
+        const isPasswordValid = bcrypt.compareSync(req.body.password,
+        user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({auth: false, token:null});
+        }
+        const token = jwt.sign({ id: user._id }, process.env.HMACSECRET, {
+            expiresIn: 8640 // expires in 24 hours
         });
-});
+        res.status(200).send({ auth: true, token: token });
+    })
+})
 
 /**
  * @argument: _id of a User
@@ -48,7 +75,29 @@ userRouter.get('/:id', function(req,res) {
 userRouter.delete('/:id', function(req,res) {
     Users.deleteOne({
         _id: req.params.id,
+    }, function(err) {
+        if (err) throw err;
     });
     res.status(200).send();
 });
+
+/**
+ * @argument: {op: ['update','remove'],
+ *             key:"field in Users",
+ *             value: "New value of above field"}
+ */
+userRouter.patch('/:id', function(req,res) {
+    const field = req.body.field;
+    const value = req.body.value;
+    const changeObj = {}
+    changeObj[field] = value;
+    Users.findOneAndUpdate({
+        _id: req.params.id}, {
+            $set : changeObj
+        },{new: true}, function(err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
 module.exports = userRouter;
--- test.js
+++ test.js
@@ -3,22 +3,10 @@ const app = require('./server');
 const expect = require('chai').expect;
 
 describe('USER ROUTE TESTS', function() {
-    it('should return array of users', function(done){
-        request(app)
-            .get('/user')
-            .set('Accept', 'application/json')
-            .expect('Content-Type',/json/)
-            .expect(200)
-            .end(function(err,res) {
-                if (err) throw err;
-                expect(res.body).to.be.an('array');
-                done();
-            })
-    });
-    let userId = null;
+    let token = null;
     it('should insert new user and than fetch the same user by id', function(done) {
         request(app)
-            .post('/user')
+            .post('/user/register')
             .set('Accept','application/json')
             .send({
                 firstName:'Shivam',
@@ -29,25 +17,62 @@ describe('USER ROUTE TESTS', function() {
             .expect(200)
             .end(function(err,res) {
                 expect(res.body).to.be.an('object');
-                userId=res.body._id;
+                expect(res.body).have.property('token');
+                expect(res.body.auth).equals(true);
+                token=res.body.token;
                 done();
             })
     });
-    it('should return new created user', function(done) {
-                request(app)
-                .get('/user/'+userId)
-                .set('Accept','application/json')
-                .expect(200)
-                .end(function(err,res) {
-                    if (err) throw err;
-                    expect(res.body).to.be.an('object');
-                    expect(res.body.firstName).eqls('Shivam');
-                    done();
-                })
+    it('should return object of user', function(done){
+        request(app)
+            .get('/user')
+            .set('Accept', 'application/json')
+            .set('x-access-token',token)
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(err,res) {
+                if (err) throw err;
+                expect(res.body).to.be.an('object');
+                expect(res.body).does.not.have.property('password');
+                done();
+            })
+    });
+    it('should return token after login', function(done){
+        request(app)
+            .get('/user/login')
+            .set('Accept', 'application/json')
+            .send({
+                email: 'shivarawat24@gmail.com',
+                password: '123',
+            })
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(err,res) {
+                if (err) throw err;
+                expect(res.body).to.be.an('object');
+                expect(res.body.auth).equals(true);
+                expect(res.body.token).to.be.an('string');
+                done();
+            })
+    });
+    it('should change firstName from Shivam to Luke', function(done) {
+        request(app)
+            .patch('/user/'+token)
+            .send({
+                op:"update",
+                field:"firstName",
+                value:"Luke"
+            })
+            .expect(200)
+            .end(function(err,res) {
+                if (err) throw err;
+                expect(res.body.firstName).equals("Luke");
+                done();
+            })
     });
     it('should return 200 status showing succ. deletion', function(done) {
         request(app)
-            .delete('/user/'+userId)
+            .delete('/user/'+token)
             .expect(200)
             .end(function(err,res) {
                 if (err) throw err;
